d4024310ba3c6f57b014750c031f34ebd54af482
Carlospotrero <42182033+Carlospotrero@users.noreply.github.com>
2018-08-16 12:07:51 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,18 @@
+#autor:carlos ruben potrero corona
+#fecha:14_ agosto_ 2018
+#actividad:trabajo paran un 10 xdxdxdxd  
+
+P=input("dame un numero : ")
+S=12
+A=2
+while not S=0: 
+ while P<S :
+  if P%2==0:
+	P=P+1
+ if  A<S-1:  
+ A=A+1
+ R=S%A
+ if R==0:
+    P=0     	
+if P==1:
+
